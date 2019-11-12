3a6b70abad2bdf3427bee21ecab665895b349a74
Nirmal kumar <nravi@Nirmals-Air.hsd1.il.comcast.net>
2019-11-11 19:37:44 -0600

    deployement

--- hello_app/app.py
+++ hello_app/app.py
@@ -6,9 +6,6 @@ from flask_restful import Resource, Api
 from flask_restful import reqparse
 import json
 
-
-
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -19,8 +16,11 @@ class HelloWorld(Resource):
         reqparser.add_argument('ln', required=False)
         args = reqparser.parse_args()
         if args['ln']:
-            url = os.environ.get('HOLA_SRV', f'http://localhost:5001/{name}')
+            url = os.environ.get('HOLA_SRV', 'http://localhost:5001/')
+            url = url+name
             r = requests.get(url)
+            if r.status_code != 200:
+                return {"error": str(r)}
             print(r)
             return json.loads(r.text)
         return {'hello': name,
@@ -36,4 +36,4 @@ api.add_resource(HelloWorld, '/<name>')
 api.add_resource(Ops, '/ops/healthz')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
--- hola_app/app.py
+++ hola_app/app.py
@@ -8,15 +8,17 @@ api = Api(app)
 
 class HollaWorld(Resource):
     def get(self, name):
-        {'hello': name,
+        return {'hola': name,
          'myip': socket.gethostbyname(socket.gethostname())}
+
 
 class Ops(Resource):
     def get(self):
         return 200
 
+
 api.add_resource(HollaWorld, '/<name>')
 api.add_resource(Ops, '/ops/healthz')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5001)
